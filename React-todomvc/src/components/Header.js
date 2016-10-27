4560d7fa1fd322ef55e51ef2b330b9acc2bdc317
redhawkIT <khodkodmitry10011983@hotmail.com>
2016-10-27 18:40:17 -0500

    convert Header to stateless component

--- React-todomvc/src/components/Header.js
+++ React-todomvc/src/components/Header.js
@@ -1,26 +1,27 @@
-import React, {PropTypes, Component} from 'react'
+import React, {PropTypes} from 'react'
 import TodoTextInput from './TodoTextInput'
 
-export default class Header extends Component {
-  static propTypes = {
-    addTodo: PropTypes.func.isRequired
-  }
+const Header = ({addTodo}) => {
 
-  handleSave = text => {
+  const handleSave = text => {
     if (text.length !== 0) {
-      this.props.addTodo(text)
+      addTodo(text)
     }
   }
 
-  render() {
-    return (
-      <header className="header">
-        <h1>todos</h1>
-        <TodoTextInput
-          newTodo
-          onSave={this.handleSave}
-          placeholder="What needs to be done?"/>
-      </header>
-    )
-  }
+  return (
+    <header className="header">
+      <h1>todos</h1>
+      <TodoTextInput
+        newTodo
+        onSave={handleSave}
+        placeholder="What needs to be done?"/>
+    </header>
+  )
 }
+
+Header.propTypes = {
+  addTodo: PropTypes.func.isRequired
+}
+
+export default Header
